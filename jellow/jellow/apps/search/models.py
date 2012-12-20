24cb599080841b1b2526edbe8e6969075ee4ca8b
Naren Sathiya <Naren Sathiya nsathiya@spawnd.com>
2012-12-20 00:26:49 -0800

    intial

--- jellow/jellow/apps/search/models.py
+++ jellow/jellow/apps/search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+
+class Article (models.Model):
+    Name = models.CharField(max_length=50)
+    Content = models.CharField(max_length=200)
+
+admin.site.register(Article)
