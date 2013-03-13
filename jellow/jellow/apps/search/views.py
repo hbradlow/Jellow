141a729d9ce5185acceff9685727a5e64dd44c4d
Naren Sathiya <Naren Sathiya nsathiya@spawnd.com>
2013-03-13 01:40:11 -0700

    few cleanup with template and search. Boost doesnt work yet

--- jellow/jellow/apps/search/views.py
+++ jellow/jellow/apps/search/views.py
@@ -9,7 +9,7 @@ def home(request):
     paragraphs = []
     if "q" in request.GET:
         sqs = SearchQuerySet().filter(content=request.GET.get('q', ''))
-        paragraphs = [a.object for a in sqs[0:30]]
+        paragraphs = [a.object for a in sqs[0:70]]
     return render_to_response("search/home.html",{"paragraphs":paragraphs},context_instance=RequestContext(request))
 
 
